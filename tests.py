016263d72d7f2ee674182d053aac065db66b6ace
Viktor Hercinger <hercinger.viktor@gmail.com>
2011-07-20 20:54:08 +0200

    test script should run examples too

--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ from unittest import *
 
 mypath = os.path.abspath(os.path.dirname(__file__))
 testdir = os.path.join(mypath, 'tests')
+exampledir = os.path.join(mypath, 'examples')
 
 sys.path.insert(0, testdir)
 
@@ -24,3 +25,20 @@ for f in os.listdir(testdir):
 
 alltests = TestSuite(suites)
 TextTestRunner(verbosity = 2).run(alltests)
+
+print >>sys.stderr, 'Running examples'
+for f in os.listdir(exampledir):
+    abspath = os.path.join(exampledir, f)
+
+    if not os.path.isdir(abspath):
+        continue
+
+    print >>sys.stderr, 'Running example %s' % f
+
+    cur = os.getcwd()
+    try:
+        os.chdir(os.path.join(exampledir, f))
+        os.system('../../cgmake -f -d')
+
+    finally:
+        os.chdir(cur)
