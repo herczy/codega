bedbfc5a982b019761e7ab1231c081c305d06a1f
Viktor Hercinger <hercinger.viktor@gmail.com>
2011-07-20 18:50:44 +0200

    added cgextra submodule for later population

---
+++ codega/cgextra/__init__.py
@@ -0,0 +1,5 @@
+'''Codegen extras, like helper functions, common generators, etc.
+
+This module is intended to have all the commonly used extras, like
+tools (indentation helpers, some features that should not be in the
+core but are to important to be left out, etc.'''
