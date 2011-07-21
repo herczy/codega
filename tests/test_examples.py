1dcda7536ef43450efe1ebaea96ed3c1f934ab15
Hercinger Viktor <hercinger.viktor@gmail.com>
2011-07-21 09:34:16 +0200

    moved example running from main test to a separate file

---
+++ tests/test_examples.py
@@ -0,0 +1,35 @@
+from unittest import TestCase, TestSuite
+import os
+import os.path
+import tempfile
+
+class TestExamples(TestCase):
+    pass
+
+def add_example_test(name, path):
+    def __run(self):
+        fd, fn = tempfile.mkstemp()
+
+        cur = os.getcwd()
+        try:
+            os.chdir(os.path.join(exampledir, f))
+            os.system('../../cgmake -f 2>%s' % fn)
+
+        finally:
+            os.chdir(cur)
+
+        data = os.read(fd, 100000)
+        self.assertNotEqual(data[:7], 'Error: ')
+
+    setattr(TestExamples, 'test_example_%s' % name, __run)
+
+mypath = os.path.abspath(os.path.dirname(__file__))
+exampledir = os.path.join(os.path.dirname(mypath), 'examples')
+
+for f in os.listdir(exampledir):
+    abspath = os.path.join(exampledir, f)
+
+    if not os.path.isdir(abspath):
+        continue
+
+    add_example_test(f, abspath)
