c8bbfc304fe72a0990e6274e14a53d571b00f913
Hercinger Viktor <hercinger.viktor@gmail.com>
2011-10-06 10:22:54 +0200

    logger.prepare may be called with fixed log level

--- codega/logger.py
+++ codega/logger.py
@@ -6,7 +6,7 @@ import sys
 from lxml.etree import use_global_python_log, PyErrorLog
 from logging import debug, info, warning, error, critical, log, DEBUG, INFO, WARNING, ERROR, CRITICAL
 
-def prepare():
+def prepare(force_level = None):
     import logging
 
     levels = {
@@ -24,25 +24,29 @@ def prepare():
 
     logging.basicConfig(format = '%(levelname) -10s %(message)s')
 
-    for ndx, opt in enumerate(sys.argv):
-        if opt == '-v' or opt == '--verbosity':
-            break
+    if force_level is None:
+        for ndx, opt in enumerate(sys.argv):
+            if opt == '-v' or opt == '--verbosity':
+                break
+
+        else:
+            ndx = -1
+
+        if ndx > 0:
+            try:
+                loglevel = levels[sys.argv[ndx + 1]]
+
+            except KeyError:
+                print >> sys.stderr, "Unknown log level %s" % opt
+                exit(1)
+
+            del sys.argv[ndx:ndx + 2]
+
+        else:
+            loglevel = ERROR
 
     else:
-        ndx = -1
-
-    if ndx > 0:
-        try:
-            loglevel = levels[sys.argv[ndx + 1]]
-
-        except KeyError:
-            print >> sys.stderr, "Unknown log level %s" % opt
-            exit(1)
-
-        del sys.argv[ndx:ndx + 2]
-
-    else:
-        loglevel = ERROR
+        loglevel = force_level
 
     logging.getLogger().setLevel(loglevel)
     use_global_python_log(PyErrorLog())
