c7d0497e4014cad837fe0ecbfd62a42430af8bd3
Hercinger Viktor <hercinger.viktor@gmail.com>
2011-11-05 18:52:17 +0100

    added template generator factory

--- codega/template.py
+++ codega/template.py
@@ -9,6 +9,8 @@ supported if needed.
 
 from decorators import abstract
 
+from generator.template import TemplateGenerator
+
 class TemplateBase(object):
     '''Base class for templates'''
 
@@ -41,3 +43,27 @@ class TemplatesetBase(object):
         '''
 
         return self.get_template(name).render(bindings)
+
+class TemplatesetGeneratorFactory(object):
+    '''
+    A factory to create TemplateGenerators based on the provided template set
+    and a template name.
+    '''
+
+    _template_set = None
+    _template_generator = None
+
+    def __init__(self, template_set, template_generator = TemplateGenerator):
+        self._template_set = template_set
+        self._template_generator = template_generator
+
+    def factory(self, name):
+        '''
+        Factory to use with the generator decorator.
+        '''
+
+        def __factory(func):
+            template = self._template_set.get_template(name)
+            return self._template_generator(template, func)
+
+        return __factory
