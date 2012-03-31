16d8019b58f8efd78d283f4201f3283696f72b67
Hercinger Viktor <hercinger.viktor@gmail.com>
2012-03-31 19:58:03 +0200

    logger.prepare works with argument too

--- codega/logger.py
+++ codega/logger.py
@@ -4,9 +4,9 @@ import traceback
 import sys
 
 from lxml.etree import use_global_python_log, PyErrorLog
-from logging import debug, info, warning, error, critical, log, DEBUG, INFO, WARNING, ERROR, CRITICAL
+from logging import debug, info, warning, error, critical, log, DEBUG, INFO, WARNING, ERROR, CRITICAL #@UnusedImports
 
-def prepare():
+def prepare(level=None):
     import logging
 
     levels = {
@@ -22,24 +22,26 @@ def prepare():
         '0' : CRITICAL,
     }
 
-    logging.basicConfig(format = '%(levelname) -10s %(message)s')
+    logging.basicConfig(format='%(levelname) -10s %(message)s')
 
-    for ndx, opt in enumerate(sys.argv):
-        if opt == '-v' or opt == '--verbosity':
-            break
+    levelname = None
+    if level is None:
+        for ndx, opt in enumerate(sys.argv):
+            if opt == '-v' or opt == '--verbosity':
+                levelname = sys.argv[ndx + 1]
+                del sys.argv[ndx:ndx + 2]
+                break
 
     else:
-        ndx = -1
+        levelname = level
 
-    if ndx > 0:
+    if levelname is not None:
         try:
-            loglevel = levels[sys.argv[ndx + 1]]
+            loglevel = levels[levelname]
 
         except KeyError:
             print >> sys.stderr, "Unknown log level %s" % opt
             exit(1)
-
-        del sys.argv[ndx:ndx + 2]
 
     else:
         loglevel = ERROR
@@ -48,8 +50,8 @@ def prepare():
     use_global_python_log(PyErrorLog())
 
 
-def exception(preface = None, level = DEBUG, short_desc = None, long_desc = None, level_trace = None, line_prefix = None):
-    exc_type, exc_value, trace = sys.exc_info()
+def exception(preface=None, level=DEBUG, short_desc=None, long_desc=None, level_trace=None, line_prefix=None):
+    exc_type, exc_value, trace = sys.exc_info() #@UnusedVariables
 
     if short_desc is None:
         short_desc = str(exc_value)
