2ca2a36c29f75aca128ecdebe58bc9517bb4e2a9
Hercinger Viktor <hercinger.viktor@gmail.com>
2012-06-14 09:45:41 +0200

    run_test.py accepts lists of modules

--- run_tests.py
+++ run_tests.py
@@ -5,5 +5,15 @@ from unittest import TestLoader, TestSuite, TextTestRunner
 
 if __name__ == '__main__':
     import tests
-    suite = TestLoader().loadTestsFromModule(tests)
+
+    loader = TestLoader()
+    suite = TestSuite()
+
+    if len(sys.argv) == 1:
+        suite.addTests(loader.loadTestsFromModule(tests))
+
+    else:
+        for mod in sys.argv[1:]:
+            suite.addTests(loader.loadTestsFromModule(__import__(mod, fromlist=['*'])))
+
     TextTestRunner(verbosity=2).run(suite)
